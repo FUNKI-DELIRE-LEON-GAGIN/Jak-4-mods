09010005fb3085b4672d86c81504c6396dde5fc9
FUNKI DELIRE (LEON GAGIN) <cabomme@gmail.com>
2025-05-04 00:22:38 +0200

    Initial commit avec suivi Git LFS

---
+++ custom_assets/jak1/levels/jump-zone/jump-zone.gd
@@ -0,0 +1,30 @@
+;; DGO definition file for Awful Village level
+;; We use the convention of having a longer DGO name for levels without precomputed visibility.
+
+;; the actual file name still needs to be 8.3
+("JZL.DGO"
+ ("test-zone-obs.o"
+ "eichar-flut+0-ag.go"
+ "flut-saddle-ag.go"
+    "flut-part.o"
+  "flutflut.o"
+  "target-flut.o"
+  "linear-plat.o"
+  "tpage-398.go"
+  "tpage-400.go"
+  "tpage-399.go"
+  "tpage-401.go"
+  "tpage-1470.go"
+  "plat-ag.go"
+  "plat-eco-ag.go"
+  "test-actor-ag.go"
+  "babak-ag.go"
+  "jump-zone.go"
+
+
+
+
+
+
+  )
+ )
---
+++ custom_assets/jak1/levels/l1/l1g.gd
@@ -0,0 +1,37 @@
+;; DGO definition file for Awful Village level
+;; We use the convention of having a longer DGO name for levels without precomputed visibility.
+
+;; the actual file name still needs to be 8.3
+("L1L.DGO"
+ ("test-zone-obs.o"
+
+
+  "linear-plat.o"
+  "tpage-398.go"
+  "tpage-400.go"
+  "tpage-399.go"
+  "tpage-401.go"
+  "tpage-1470.go"
+  "plat-ag.go"
+  "plat-eco-ag.go"
+  "test-actor-ag.go"
+  "babak-ag.go"
+
+  
+
+
+
+
+  "balloonlurker.o"
+  "balloonlurker-ag.go"
+  "puffer.o"
+  "target-tube.o"
+  "eichar-tube+0-ag.go"
+  
+
+
+
+
+  "l1.go"
+  )
+ )
